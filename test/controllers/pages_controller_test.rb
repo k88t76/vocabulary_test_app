44a76e9e2f628f7893a2d9650fc242376b3f7916
Ubuntu <ubuntu@ip-172-31-17-232.us-east-2.compute.internal>
2020-05-13 07:35:56 +0000

    Add a Pages controller

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get output" do
+    get pages_output_url
+    assert_response :success
+  end
+
+end
